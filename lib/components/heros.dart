df007bd0648d461c4e49ec3448c15580d42058ca
songchez <tama4840@email.com>
2023-01-23 12:35:25 +0900

    layout

--- lib/components/heros.dart
+++ lib/components/heros.dart
@@ -2,19 +2,144 @@ import 'package:flutter/material.dart';
 
 class Heros extends StatelessWidget {
   const Heros({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
+        body: Column(children: <Widget>[
+      Expanded(
+        flex: 2,
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Business Consulting',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              const Text(
+                'We help businesses succeed',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      Expanded(
+          flex: 1,
           child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.blue,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(children: <Widget>[
+                    const SizedBox(height: 30.0),
+                    const Text(
+                      'What We Offer',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    Container(
+                      width: double.infinity,
+                      height: 1.0,
+                      color: Colors.grey[300],
+                    ),
+                    const SizedBox(height: 20.0),
+                    ServiceCard(
+                      icon: Icons.business_center,
+                      title: 'Business Strategy',
+                      description:
+                          'We help businesses develop a clear and effective strategy to reach their goals.',
+                    ),
+                    ServiceCard(
+                      icon: Icons.monetization_on,
+                      title: 'Financial Management',
+                      description:
+                          'We provide financial management services to help businesses make informed decisions.',
+                    ),
+                    ServiceCard(
+                      icon: Icons.people,
+                      title: 'Human Resources',
+                      description:
+                          'We help businesses attract, retain, and manage top talent to drive success.',
+                    ),
+                    const SizedBox(height: 20.0),
+                    GestureDetector(
+                        onTap: () {
+                          'Learn More button clicked';
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          height: 50.0,
+                        ))
+                  ]))))
+    ]));
+  }
+}
+
+class ServiceCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String description;
+
+  ServiceCard(
+      {required this.icon, required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: SizedBox(
+        width: double.infinity,
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
             child: Column(
-              children: const [Text("뭔디")],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  size: 50.0,
+                  color: Colors.grey[600],
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  description,
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 14.0,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sehan/components/Heros.dart';
 
 void main() {
   runApp(const MyApp());
@@ -76,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("정보를 입력하고 기업진단 해보세요!"),
+                const Text("정보를 입력하고 기업진단 해보세요!"),
                 const TextField(
                   decoration: InputDecoration(
                       label: Text("건설종목"), border: OutlineInputBorder()),
@@ -124,153 +123,6 @@ class _MyHomePageState extends State<MyHomePage> {
           child: const Text('Page 3'),
         ),
       ][currentPageIndex],
-    );
-  }
-}
-
-class BusinessConsultingPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(children: <Widget>[
-      Expanded(
-        flex: 2,
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/background.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Business Consulting',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                'We help businesses succeed',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      Expanded(
-          flex: 1,
-          child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
-                ),
-              ),
-              child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Column(children: <Widget>[
-                    SizedBox(height: 30.0),
-                    Text(
-                      'What We Offer',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                    Container(
-                      width: double.infinity,
-                      height: 1.0,
-                      color: Colors.grey[300],
-                    ),
-                    SizedBox(height: 20.0),
-                    ServiceCard(
-                      icon: Icons.business_center,
-                      title: 'Business Strategy',
-                      description:
-                          'We help businesses develop a clear and effective strategy to reach their goals.',
-                    ),
-                    ServiceCard(
-                      icon: Icons.monetization_on,
-                      title: 'Financial Management',
-                      description:
-                          'We provide financial management services to help businesses make informed decisions.',
-                    ),
-                    ServiceCard(
-                      icon: Icons.people,
-                      title: 'Human Resources',
-                      description:
-                          'We help businesses attract, retain, and manage top talent to drive success.',
-                    ),
-                    SizedBox(height: 20.0),
-                    GestureDetector(
-                        onTap: () {
-                          print('Learn More button clicked');
-                        },
-                        child: Container(
-                          width: double.infinity,
-                          height: 50.0,
-                        ))
-                  ]))))
-    ]));
-  }
-}
-
-class ServiceCard extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String description;
-
-  ServiceCard(
-      {required this.icon, required this.title, required this.description});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20.0),
-      child: SizedBox(
-        width: double.infinity,
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Icon(
-                  icon,
-                  size: 50.0,
-                  color: Colors.grey[600],
-                ),
-                SizedBox(height: 10.0),
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                Text(
-                  description,
-                  style: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 14.0,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
